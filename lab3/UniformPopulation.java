0fcf93cd9b15764c741160e0815ccfa1d21c13a3
Winnaries <Nattawat Pornthisan>
2022-10-09 12:24:09 +0200

    feat(lab3): Add lock-free skip-list benchmark code

--- lab3/UniformPopulation.java
+++ lab3/UniformPopulation.java
@@ -1,13 +1,22 @@
+import java.util.Random;
+
 public class UniformPopulation implements Population {
+    Random rng; 
+    int min, max; 
+    int count = 0; 
 
     public UniformPopulation(int seed, int min, int max) {
+        this.min = min; 
+        this.max = max; 
+        this.rng = new Random(seed);
     }
 
     public int getCount() {
-        return 0;
+        return count; 
     }
 
     public int getSample() {
-        return 0;
+        count += 1; 
+        return (int)(rng.nextDouble() * (max - min) + min); 
     }
 }
