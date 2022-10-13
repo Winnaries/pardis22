1fed5a56477c837fe903a131e10802e34b5eac23
Winnaries <Nattawat Pornthisan>
2022-10-13 22:56:27 +0200

    repo(lab3): Fix typo in run.sh

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 javac **/*.java
-java $impl.LockFreeSkipListTest $DIST $CONTAINS $ADD $REMOVE $NTHREADS $MAX $NOPS 2> err.log
-java $impl.LockFreeSkipListTest $DIST $CONTAINS $ADD $REMOVE $NTHREADS $MAX $NOPS 2> err.log
-java $impl.LockFreeSkipListTest $DIST $CONTAINS $ADD $REMOVE $NTHREADS $MAX $NOPS 2> err.log
+java $IMPL.LockFreeSkipListTest $DIST $CONTAINS $ADD $REMOVE $NTHREADS $MAX $NOPS 2> err.log
+java $IMPL.LockFreeSkipListTest $DIST $CONTAINS $ADD $REMOVE $NTHREADS $MAX $NOPS 2> err.log
+java $IMPL.LockFreeSkipListTest $DIST $CONTAINS $ADD $REMOVE $NTHREADS $MAX $NOPS 2> err.log
